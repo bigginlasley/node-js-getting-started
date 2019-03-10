92d369a911641cf155832de3209352601de29319
Biggin <bigginlasley@gmail.com>
2019-03-09 18:37:11 -0700

    testing

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('public/index'))
   .get('/postage', handler)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
